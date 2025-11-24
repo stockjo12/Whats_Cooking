13613a0c5e2a62c2987ca184477faf1c468b3f71
stockjo12 <stockjo12@gmail.com>
2025-11-24 11:47:14 -0700

    Initializing
    Made files for Kaggle Competition

---
+++ Cooking.R
@@ -0,0 +1,49 @@
+### SET UP ###
+#Downloading libraries
+library(tidyverse)
+library(tidymodels)
+library(beepr)
+library(vroom)
+library(janitor)
+library(doParallel)
+
+#Bringing in Data
+setwd("~/Library/CloudStorage/OneDrive-BrighamYoungUniversity/STAT 348/Coding/Walmart")
+
+### FEATURE ENGINEERING ###
+#Making Recipe
+
+
+### WORK FLOW ###
+# X Model
+#Defining Model
+
+
+#Creating Workflows
+
+
+### CROSS VALIDATION ###
+#Defining Grids of Values
+
+
+#Splitting Data
+
+
+#Run Cross Validations
+
+
+#Find Best Tuning Parameters
+
+
+#Finalizing Workflow
+
+
+### SUBMISSION ###
+#Making Predictions
+
+
+#Formatting Predictions for Kaggle
+
+
+#Saving CSV File
+
